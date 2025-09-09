2eca12b4ae335edca6392d3cb99d20a6284d6427
thedudeleo <smithbros.leo@gmail.com>
2025-09-09 09:52:24 +0200

    made slider work

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-﻿document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+﻿
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
@@ -8,3 +10,23 @@
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+        loop: true,
+        autoHeight: true,
+        autoplay: {
+            delay: 5000,
+        },
+
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+  
+    });
+});
